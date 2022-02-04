7420a14734fe77bdfffcb44a96b56f4610d8ecfd
yslinwe <36142401+yslinwe@users.noreply.github.com>
2022-02-04 16:24:38 +0800

    'update'

--- create.py
+++ create.py
@@ -84,8 +84,8 @@ def getLink(video_path):
         -map [v1out] -c:v:0 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:0 5M -maxrate:v:0 6M -minrate:v:0 3M -bufsize:v:0 12M -preset slow -g 48 -sc_threshold 0 -keyint_min 48 \
         -map [v2out] -c:v:1 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:1 3M -maxrate:v:1 4M -minrate:v:1 1.5M -bufsize:v:1 8M -preset slow -g 48 -sc_threshold 0 -keyint_min 48 \
         -map [v3out] -c:v:2 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:2 1M -maxrate:v:2 2M -minrate:v:2 0.5M -bufsize:v:2 4M -preset slow -g 48 -sc_threshold 0 -keyint_min 48 \
-        -map [v4out] -c:v:3 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:2 0.8M -maxrate:v:2 1M -minrate:v:2 0.4M -bufsize:v:2 2M -preset slow -g 48 -sc_threshold 0 -keyint_min 48 \
-        -map [v5out] -c:v:4 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:2 0.6M -maxrate:v:2 0.7M -minrate:v:2 0.3M -bufsize:v:2 1M -preset slow -g 48 -sc_threshold 0 -keyint_min 48 \
+        -map [v4out] -c:v:3 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:3 0.8M -maxrate:v:3 1M -minrate:v:3 0.4M -bufsize:v:3 2M -preset slow -g 48 -sc_threshold 0 -keyint_min 48 \
+        -map [v5out] -c:v:4 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:4 0.6M -maxrate:v:4 0.7M -minrate:v:4 0.3M -bufsize:v:4 1M -preset slow -g 48 -sc_threshold 0 -keyint_min 48 \
         -map a:0 -c:a:0 aac -b:a:0 96k -ac 2 \
         -map a:0 -c:a:1 aac -b:a:1 96k -ac 2 \
         -map a:0 -c:a:2 aac -b:a:2 48k -ac 2 \
